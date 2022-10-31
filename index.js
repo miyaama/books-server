8c59c89bc072465a59d7531c6c519f01490936a9
Anna Obikhodova <a.obikhodova@andersenlab.com>
2022-10-31 13:43:04 +0400

    fix

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
-const mysql = require("mysql2");
 const cors = require("cors");
 app.use(express.json());
 app.use(cors());
